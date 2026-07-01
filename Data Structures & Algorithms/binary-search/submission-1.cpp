0479890e3bc413657403461dbe621f778aeec958
kraken1819 <92370732+kraken1819@users.noreply.github.com>
2026-07-01 12:18:08 +0530

    Add: binary-search - submission-1

---
+++ Data Structures & Algorithms/binary-search/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size(); 
+
+
+        int l = 0; 
+        int h = n-1; 
+
+        int m; 
+
+        while( l <= h ){
+            m = l + ( h - l ) / 2; 
+
+            if( nums[m] == target ){
+                return m; 
+            }else if( nums[m] > target){
+                h = m -1 ; 
+            }else { 
+                l = m + 1 ; 
+            }
+        }
+        return -1; 
+    }
+};
